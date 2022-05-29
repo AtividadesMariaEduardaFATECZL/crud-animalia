a2e8a43fa4cd9bc02d92f3dfe7ab3b4fd90dd4c3
mariasantosdev <mariaeduardasrodolfo2@gmail.com>
2022-05-28 21:35:15 -0300

    deletando as classes main usadas para testar a camada de persistencia, alterando gitignore e criando classe de teste para as validacoes

---
+++ src/test/java/com/animalia/crudanimalia/model/utils/ValidatorUtilsTest.java
@@ -0,0 +1,25 @@
+package com.animalia.crudanimalia.model.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
+
+import static com.animalia.crudanimalia.model.utils.ValidadorUtils.cantBeNull;
+import static com.animalia.crudanimalia.model.utils.ValidadorUtils.cantBeNullOrEmpty;
+
+public class ValidatorUtilsTest {
+
+    @ParameterizedTest
+    @NullSource
+    void should_throw_exception_when_field_is_null(String field) {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> cantBeNull(field));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void should_throw_exception_when_field_is_null_and_empty(String field) {
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> cantBeNullOrEmpty(field));
+    }
+}
